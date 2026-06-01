87e33df244d9fe2df62b7efa725441067232f56b
AnujKumar8400 <anujk21615@gmail.com>
2026-06-01 12:56:18 +0530

    Updated templates and improved UI

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Blog_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Blog_Project/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
