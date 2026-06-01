d5317bd0fd7605d713f4c50160afd938f70f1b62
AnujKumar8400 <anujk21615@gmail.com>
2026-06-01 11:45:20 +0530

    Fix ALLOWED_HOSTS

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-je@c#)v__4hj5j0y&(7ymc7pp$nrc99ps==l3egg!4p10ns^i8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "anujkumar.pythonanywhere.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
